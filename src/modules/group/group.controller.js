36216c811daffcd48f752b9996f639798815924a
TLCRATEAM <64020797+TLCRATEAM@users.noreply.github.com>
2022-11-20 21:20:18 +0700

    update[group]: get all groups of current user

--- src/modules/group/group.controller.js
+++ src/modules/group/group.controller.js
@@ -3,19 +3,51 @@ const db = require('#common/database/index.js')
 // Create main Model
 const Group = db.Group
 const User_Group = db.User_Group
+const Role = db.Role
 
 // Main work
 
 const getAllGroup = async (req, res) => {
-    const groups = await Group.findAll({ attributes: ['id', 'name'] })
+    console.log('tests:', req.user.id)
+    const groups = await Group.findAll({
+        include: {
+            model: User_Group,
+            as: 'participants',
+            // where: { user_id: req.user.id },
+            where: { user_id: req.user.id },
+
+            // include: ['role'],
+            // include: {
+            //     model: Role,
+            //     as: 'role',
+            // },
+        },
+    })
+    for (const group of groups) {
+        // const meInGroup = group.dataValues.participants.find(
+        //     (participant) => participant.user_id === req.user.id
+        // )
+        // if (meInGroup) {
+        //     group.dataValues['my_role'] = meInGroup.dataValues.role_id
+        //     group.dataValues['group_size'] = group.dataValues.participants.length
+        // }
+
+        const size = await User_Group.count({
+            where: { group_id: group.dataValues.id },
+        })
+
+        group.dataValues['my_role'] = group.dataValues.participants[0].role_id
+        group.dataValues['group_size'] = size
+        // console.log(group)
+    }
 
     res.status(200).send({ data: groups })
 }
 
-const getUserGroups = async (req, res) => {
-    const userId = req.params.userId
-    const userGroups = await Group.findAll({ include: [User] })
-}
+// const getUserGroups = async (req, res) => {
+//     const userId = req.params.userId
+//     const userGroups = await Group.findAll({ include: [User] })
+// }
 
 const getGroup = async (req, res) => {
     const id = req.params.id
@@ -68,6 +100,11 @@ const deleteGroup = async (req, res) => {
     await Group.destroy({ where: { id: id } })
 
     res.status(200).send({ message: `Group is deleted` })
+}
+
+const joinGroupByLink = async () => {
+    const groupId = req.params.groupId
+    const userId = req.params.userId
 }
 
 module.exports = { getAllGroup, getGroup, addGroup, updateGroup, deleteGroup }
