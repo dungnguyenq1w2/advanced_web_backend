9336b2d5ac016761113abe0be3ffe23012cd7f97
hdDuy0505 <92098917+hdDuy0505@users.noreply.github.com>
2022-11-26 17:23:00 +0700

    update email content

--- src/common/config/mail.config.js
+++ src/common/config/mail.config.js
@@ -7,11 +7,11 @@ const ENCRYPTION = process.env.MAIL_ENCRYPTION
 const FROM_ADDRESS = process.env.MAIL_FROM_ADDRESS
 const FROM_NAME = process.env.MAIL_FROM_NAME
 const htmlContent = (user, hashedEmail) => {
-    return `<a href="${process.env.APP_FRONT_END_URL}/auth/verify?email=${user}&token=${hashedEmail}`
+    return `<a href="${process.env.APP_FRONT_END_URL}/auth/verify?email=${user}&token=${hashedEmail}>Click here to verify</a>`
 }
 
 const htmlContentInviteGroup = (groupId, email, token) => {
-    return `<a href="${process.env.APP_FRONT_END_URL}/group/${groupId}/invite?email=${email}&token=${token}`
+    return `<a href="${process.env.APP_FRONT_END_URL}/group/${groupId}/invite?email=${email}&token=${token}>Click here to join group</a>`
 }
 
 module.exports = {
