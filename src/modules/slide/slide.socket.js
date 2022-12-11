33007afba845ac1b12b0430e6add93b652470384
Nguyễn Tấn Dũng <dungnguyenq1w2@gmail.com>
2022-12-11 17:56:13 +0700

    done[slide]: done realtime result

--- src/modules/slide/slide.socket.js
+++ src/modules/slide/slide.socket.js
@@ -3,15 +3,19 @@ const db = require('#common/database/index.js')
 const User_Choice = db.User_Choice
 
 const slideSocket = (io, socket) => {
-    socket.on('client-send-choices', async (slideId, userId, choices) => {
+    socket.on('client-send-choices', async (slideId, guestId, choices) => {
+        console.log('🚀 ~ guestId', guestId)
+        console.log('🚀 ~ choices', choices)
         io.of('/guest').to(`slide${slideId}`).emit('server-send-choices', choices)
         io.of('/host').to(`slide${slideId}`).emit('server-send-choices', choices)
 
-        // const newUserChoice = {
-        //     user_id: userId,
-        //     choice_id: choiceId,
-        // }
-        // await User_Choice.create(newUserChoice)
+        for (const choiceId of choices) {
+            const newUserChoice = {
+                guest_id: guestId,
+                choice_id: choiceId,
+            }
+            await User_Choice.create(newUserChoice)
+        }
     })
 }
 
